c5175bf5d222158483cdcc8d0efb0eaecad65798
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-08-23 00:34:57 -0300

    randomport

---
+++ twFilmes/src/test/java/br/gj/infnetat/twfilmes/TwFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gj.infnetat.twfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwFilmesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

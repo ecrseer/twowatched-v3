ccb06db693a32466657ed89fe83c28d5c241c90b
Gabriel Justino Marques Couto <juustino@live.com>
2024-08-31 00:26:48 -0300

    moving projects

--- twFilmes/src/main/java/br/gj/infnetat/twfilmes/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.gj.infnetat.twfilmes.exceptions;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- twPosts/src/main/java/br/gj/infnetat/twposts/services/ComentarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.gj.infnetat.twposts.services;
-
-import br.gj.infnetat.twposts.model.Comentario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
-}

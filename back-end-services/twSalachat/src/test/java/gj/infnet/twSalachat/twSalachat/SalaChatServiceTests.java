08e631619de325305685a2e7ad9eb88df85cd2e9
Gabriel Justino Marques Couto <juustino@live.com>
2024-09-26 22:00:28 -0300

    twposts docker

--- back-end-services/twSalachat/src/test/java/gj/infnet/twSalachat/twSalachat/SalaChatServiceTests.java
+++ back-end-services/twSalachat/src/test/java/gj/infnet/twSalachat/twSalachat/SalaChatServiceTests.java
@@ -15,25 +15,5 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 public class SalaChatServiceTests {
 
-    @Autowired
-    private SalaChatServiceImpl salaChatService;
-
-
-    @Test
-    @DisplayName("cadastra sala")
-    public void testaCadastraSala() {
-        SalaChat salvo = salaChatService.save(new SalaChat("Sala um"));
-        assertNotNull(salvo);
-
-        Long filme = 1L;
-        SalaChat salaComFilme = new SalaChat("Sala Com filme", filme);
-        SalaChat salvoComFilme = salaChatService.save(salaComFilme);
-        assertNotNull(salvoComFilme);
-
-        Long usuarioSalvo = 1L;
-        Long outroUsuario = 2L;
-        SalaChat salaComFilmeEUsuario = new SalaChat("Sala Com filme e usuario", filme, List.of(usuarioSalvo, outroUsuario));
-        SalaChat salvoComFilmeUsuario = salaChatService.save(salaComFilmeEUsuario);
-        assertNotNull(salvoComFilmeUsuario);
-    }
+     
 }
